1f684f4082537346f9a4874afad037d695af7a34
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-01-15 15:53:40 +0300

    changed eslint

--- ProjectCalendar/Frontend/.eslintrc.js
+++ ProjectCalendar/Frontend/.eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   "plugins": [
     "react-redux"
   ],
-  "extends": "airbnb",
-  "plugins": ["react"],
-  "parserOptions": {
-    "ecmaVersion": 6
-  },
+  // "extends": "airbnb",
+  // "plugins": ["react"],
+  // "parserOptions": {
+  //   "ecmaVersion": 6
+  // },
   "rules": {
     "react-redux/connect-prefer-named-arguments": 2
   }
