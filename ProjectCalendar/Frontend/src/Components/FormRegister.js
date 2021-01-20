681681f102117b282806faafa0a41aa2a69ce33c
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-01-20 17:21:10 +0300

    2Error

--- ProjectCalendar/Frontend/src/Components/FormRegister.js
+++ ProjectCalendar/Frontend/src/Components/FormRegister.js
@@ -6,62 +6,60 @@ import { addUser } from '../Actions';
 import paths from '../Routes/paths';
 
 const FormRegister = () => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        const {
-            target: {
-                children: {
-                    password: {
-                        value: password },
-                    confirmPassword: {
-                        value: confirmPassword },
-                    name: {
-                        value: name },
-                    email: {
-                        value: email },
-                }
+    const {
+        target: {
+            children: {
+                password: {
+                    value: password },
+                confirmPassword: {
+                    value: confirmPassword },
+                name: {
+                    value: name },
+                email: {
+                    value: email },
             }
-        } = event;
-
-        if (password === confirmPassword) {
-            dispatch(addUser(name, email, password));
-            history.push(paths.calendar);
         }
+  } = e;
+
+  if (password === confirmPassword) {
+        dispatch(addUser(name, email, password));
+        history.push(paths.calendar);
     }
+  }
 
-    const onChange = (event) => {
-        const {
-            target: {
-                name, value }
-        } = event;
+  const onChange = (event) => {
+    const {
+        target: { name, value } } = event;
 
-        switch (name) {
-            case 'name':
-                setName(value);
-                break
-            case 'email':
-                setEmail(value);
-                break
-            case 'password':
-                setPassword(value);
-                break
-            case 'confirmPassword':
-                setConfirmPassword(value);
-                break
-            default:
-                break
-        }
+    switch (name) {
+        case 'name':
+            setName(value);
+            break
+        case 'email':
+            setEmail(value);
+            break
+        case 'password':
+            setPassword(value);
+            break
+        case 'confirmPassword':
+            setConfirmPassword(value);
+            break
+        default:
+            break
     }
+  }
 
-    return (
+  return (
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} type="text" placeholder="Name" name="name" value={name} />
@@ -70,7 +68,8 @@ const FormRegister = () => {
                 <input onChange={onChange} type="password" placeholder="Confirm password" name="confirmPassword" value={confirmPassword} />
                 <button type='submit'>Sign up </button>
             </form>
-        </div>)
+        </div>
+    )
 }
 
 export default FormRegister;
