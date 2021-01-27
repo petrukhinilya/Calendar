4d232409710844bf34ce6b8ff696e05653797693
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-01-27 12:31:36 +0300

    Remove tabs

--- ProjectCalendar/Frontend/src/Components/Calendar/CreateCalendar.js
+++ ProjectCalendar/Frontend/src/Components/Calendar/CreateCalendar.js
@@ -1,30 +1,28 @@
 import moment from 'moment';
 
 export default function createCalendar(day, a, dayStyles, setValue) {
-    const newArr = [];
+  const newArr = [];
 
-    for (let i = 0; i < a.length; i++) {
-        let dayStart = day.format('DD MMM YYYY');
-        let bStart = moment(a[i].startDate).format('DD MMM YYYY');
-        if (dayStart === bStart) {
-            newArr.push(a[i])
-        }
+  for (let i = 0; i < a.length; i++) {
+    let dayStart = day.format('DD MMM YYYY');
+    let bStart = moment(a[i].startDate).format('DD MMM YYYY');
+    if (dayStart === bStart) {
+      newArr.push(a[i])
     }
+  }
 
-    return (
+  return (
         <div className='day' onClick={() => setValue(day)} >
-            <div id='number'>
-                <div className={dayStyles(day)}>{day.format('D').toString()}</div>
-                <div>{newArr.map((event) => {
-                    return (
-                        <div className='highlight'>{event.event}</div>
-                    )
+          <div id='number'>
+            <div className={dayStyles(day)}>{day.format('D').toString()}</div>
+            <div>{newArr.map((event) => {
+               return (
+                  <div className='highlight'>{event.event}</div>
+                )
                 })
-                }
-                </div>
+            }
             </div>
+          </div>
         </div>
-    )
-
-
+  )
 }
