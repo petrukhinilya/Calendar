3b565d9318b8625d12ab7575c66cef929b458db1
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-02-02 12:14:12 +0300

    Fix format with space

--- ProjectCalendar/Frontend/src/Components/Calendar/CreateCalendar.js
+++ ProjectCalendar/Frontend/src/Components/Calendar/CreateCalendar.js
@@ -1,12 +1,12 @@
 import moment from 'moment';
 
-export default function createCalendar(day, getallEvents, dayStyles, setValue) {
+export default function createCalendar(day, getallEvents, dayStyles, setValue, deleteEvent, setChangePopup, setEvent) {
   const allEvents = [];
 
   for (let i = 0; i < getallEvents.length; i++) {
-    let dayStart = day.format('DD MMM YYYY');
-    let dayStartEvent = moment(getallEvents[i].startDate).format('DD MMM YYYY');
-    let dayEndEvent = moment(getallEvents[i].endDate).format('DD MMM YYYY');
+    let dayStart = day.format('MM DDD YYYY');
+    let dayStartEvent = moment(getallEvents[i].startDate).format('MM DDD YYYY');
+    let dayEndEvent = moment(getallEvents[i].endDate).format('MM DDD YYYY');
 
     if (dayStart >= dayStartEvent && dayStart <= dayEndEvent) {
       allEvents.push(getallEvents[i]);
@@ -17,12 +17,25 @@ export default function createCalendar(day, getallEvents, dayStyles, setValue) {
     <div className='day' onClick={() => setValue(day)} >
       <div id='number'>
         <div className={dayStyles(day)}>{day.format('D').toString()}</div>
+        <div>
           {allEvents.map((event) => {
+              const deleteEvents = () => {
+                deleteEvent(event._id);
+              }
+              const updateEvents = () => {
+                setEvent(event);
+                setChangePopup(true);
+              }
+
             return (
-              <div className='highlight'>{event.event}</div>
+              <div className='highlight'>
+                <div className = 'event' onClick={updateEvents}>{event.event}</div>
+                <button className='del' onClick={deleteEvents}>Del</button>
+              </div>
               )
             })
           }
+        </div>
       </div>
     </div>
   )
