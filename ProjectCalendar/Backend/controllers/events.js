85777e0364ccacfa79c43814287dd6e295460b45
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-02-08 17:47:51 +0300

    Change middleware function

--- ProjectCalendar/Backend/controllers/events.js
+++ ProjectCalendar/Backend/controllers/events.js
@@ -1,10 +1,9 @@
 const Events = require('../models/events');
-const jwt = require('jsonwebtoken');
 
 module.exports = {
     create_event: function (req, res, next) {
-        const { startDate, endDate, event, created_by } = req.body;
-        
+        const { startDate, endDate, event, id:created_by } = req.body;
+
         Events.create({ startDate, endDate, event, created_by }, function (err, result) {
             if (err) {
                 console.log("error", err)
@@ -15,8 +14,7 @@ module.exports = {
         })
     },
     get_events: function (req, res, next) {
-        Events.find({}, function (err, events) {
-
+        Events.find({created_by:req.body.id}, function (err, events) {
             if (err) {
                 console.log(err)
                 return next(err)
@@ -32,7 +30,7 @@ module.exports = {
         })
     },
     update_event: function (req, res, next) {
-        const { startDate, endDate, event, created_by } = req.body;
+        const { startDate, endDate, event, id:created_by } = req.body;
         Events.updateOne({ _id: req.params.id }, { startDate, endDate, event, created_by },
             function (err) {
                 if (err) return next(err);
--- ProjectCalendar/Backend/routes/events.js
+++ ProjectCalendar/Backend/routes/events.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const verify = require('../middleware/verifyToken')
 
 router.post('/add', verify.tokenVerify, event_controller.create_event);
-router.get('/get', verify.queryVerify, event_controller.get_events);
+router.get('/get', verify.tokenVerify, event_controller.get_events);
 router.delete('/:id/delete', verify.tokenVerify, event_controller.delete_event);
 router.put('/:id/update', verify.tokenVerify, event_controller.update_event);
 
