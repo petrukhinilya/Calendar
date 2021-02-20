1a0ce198098113bd5d89c2c0c7158c90d38e3832
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-02-20 13:51:49 +0300

    Redirect from base route

--- ProjectCalendar/Frontend/src/App.js
+++ ProjectCalendar/Frontend/src/App.js
@@ -6,7 +6,6 @@ import Main from './Routes/Main'
 
 function App() {
   return (
-
     <Main />
   )
 }
--- ProjectCalendar/Frontend/src/Routes/Main.js
+++ ProjectCalendar/Frontend/src/Routes/Main.js
@@ -1,25 +1,26 @@
-import React , {useEffect} from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import Calendar from '../Pages/Calendar'
-import LoginPage from '../Pages/LoginPage'
-import RegistrationPage from '../Pages/RegistrationPage'
-import paths from './paths'
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Calendar from '../Pages/Calendar';
+import LoginPage from '../Pages/LoginPage';
+import RegistrationPage from '../Pages/RegistrationPage';
+
+import paths from './paths';
 import ProtectedRoute from './ProtectedRoute';
 
-
 function Main() {
- 
-    const { login, registration, calendar } = paths
-    return (
-        <main>     
-            <Switch>
-                
-                <Route  path={login} component={LoginPage} />
-                <Route path={registration} component={RegistrationPage} />
-                <ProtectedRoute path={calendar} component={Calendar} />
-            </Switch>
-        </main>
-    )
+	const { login, registration, calendar } = paths
+
+	return (
+		<main>
+			<Redirect from="/" to={calendar} />
+			<Switch>
+				<Route path={login} component={LoginPage} />
+				<Route path={registration} component={RegistrationPage} />
+				<ProtectedRoute path={calendar} component={Calendar} />
+			</Switch>
+		</main>
+	)
 }
 
-export default Main
+export default Main;
