5752e1825b7d8b063ee078f3c0ef450b9c4527ec
petrukhinilya <petrukhinilya.exceed@gmail.com>
2021-03-16 10:41:14 +0300

    Add toolbar and try to interact with data

--- ProjectCalendar/Frontend/src/App.js
+++ ProjectCalendar/Frontend/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import {
   Scheduler,
-  WeekView,
+  MonthView,
+  Toolbar,
+  DateNavigator,
   Appointments,
-  MonthView
+  TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 import Paper from '@material-ui/core/Paper';
@@ -14,18 +16,31 @@ import { ViewState } from '@devexpress/dx-react-scheduler';
 import Main from './Routes/Main';
 
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const Aevent = [{
+    title: 'Book Flights to San Fran for Sales Trip',
+    startDate: new Date(2021, 2, 23, 12, 0),
+    endDate: new Date(2021, 2, 23, 13, 0),
+  }]
+  const event = useSelector(state => state.events.events)
+  const[d,setD] = useState(event)
+
+  console.log(event)
   return (
     <>
       {/* <Redirect from='/' to='/login'/> */}
       <Paper>
-    <Scheduler>
-      <ViewState/>
-      <MonthView />
-      <Appointments />
-    </Scheduler>
-  </Paper>
+        <Scheduler data={Aevent}>
+          <ViewState />
+          <MonthView />
+          <Toolbar />
+          <DateNavigator />
+          <TodayButton />
+          <Appointments />
+        </Scheduler>
+      </Paper>
       <Main />
     </>
   )
